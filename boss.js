cd47cecc8484f9d4389fe3c2bdfe3cf2af794121
Matt Richardson <matt.richardson@focusrite.com>
2021-05-12 15:44:45 +0100

    add boss

--- boss.js
+++ boss.js
@@ -3,12 +3,22 @@ import { Vec2 } from './vec2.js';
 
 export class Boss {
   constructor() {
-    this.position = new Vec2(WIDTH / 2 - BOSS_SIZE / 2, HEIGHT / 2 - BOSS_SIZE / 2);
+    this.position = new Vec2(
+      WIDTH / 2 - BOSS_SIZE / 2,
+      HEIGHT / 2 - BOSS_SIZE / 2
+    );
     this.health = 100;
+    this.image = new Image(BOSS_SIZE, BOSS_SIZE);
+    this.image.src = './boss.png';
   }
 
   draw = (ctx) => {
-    ctx.fillStyle = '#B3A712';
-    ctx.fillRect(this.position.x, this.position.y, BOSS_SIZE, BOSS_SIZE);
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      BOSS_SIZE,
+      BOSS_SIZE
+    );
   };
 }
